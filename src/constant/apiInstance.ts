b57598ff630fd4a941df31597ea7d5395a7f6f25
Pham Duc Phuong <phuong.mom33@gmail.com>
2024-03-06 01:14:34 +0700

    s

--- src/constant/apiInstance.ts
+++ src/constant/apiInstance.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestHeaders, CreateAxiosDefaults } from 'axios'
 import { getID, getToken } from 'utils'
 
-const tokenCybersoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJCb290Y2FtcCA0NyIsIkhldEhhblN0cmluZyI6IjE1LzAyLzIwMjQiLCJIZXRIYW5UaW1lIjoiMTcwNzk1NTIwMDAwMCIsIm5iZiI6MTY3ODk4NjAwMCwiZXhwIjoxNzA4MTAyODAwfQ.YGLcwOu11pM9sD9C2a0dia7O_6vvsYwkCoo1sqcbCFM"
+const tokenCybersoft = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0ZW5Mb3AiOiJOb2RlSlMgMzgiLCJIZXRIYW5TdHJpbmciOiIxNC8wNy8yMDI0IiwiSGV0SGFuVGltZSI6IjE3MjA5MTUyMDAwMDAiLCJuYmYiOjE3MDI0ODY4MDAsImV4cCI6MTcyMTA2MjgwMH0.cB5XSbdlq0lzL-wmbcuAyvlRLMYFWmr20ODRWN5rPZc"
 export const apiInstance = (config?: CreateAxiosDefaults) => {
     const api = axios.create(config)
     api.interceptors.request.use((config) => {
